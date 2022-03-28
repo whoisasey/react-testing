399a3245e087b884ff6878d84bb947ead7ec0122
whoisasey <atinachang@gmail.com>
2022-03-28 16:34:44 -0400

    organize into describe blocks

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -9,16 +9,20 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
 );
 
 describe('Todo Footer', () => {
-  it('should render correct amount of incomplete tasks', () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-    const paragraphElement = screen.getByText(/5 tasks left/i);
-    expect(paragraphElement).toBeInTheDocument();
+  describe('Multiple incomplete tasks', () => {
+    it('should render correct amount of incomplete tasks', () => {
+      render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+      const paragraphElement = screen.getByText(/5 tasks left/i);
+      expect(paragraphElement).toBeInTheDocument();
+    });
   });
 
-  it('should render "task" when the number of incomplete tasks is 1', () => {
-    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-    const paragraphElement = screen.getByText(/1 task left/i);
-    expect(paragraphElement).toBeInTheDocument();
+  describe('Single incomplete task', () => {
+    it('should render "task" when the number of incomplete tasks is 1', () => {
+      render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+      const paragraphElement = screen.getByText(/1 task left/i);
+      expect(paragraphElement).toBeInTheDocument();
+    });
   });
 
   // other assertions
