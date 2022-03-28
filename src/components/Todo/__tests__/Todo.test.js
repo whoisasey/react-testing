428b61a6013c28c4d092cf7064c97243b64b20c7
whoisasey <atinachang@gmail.com>
2022-03-28 17:19:28 -0400

    add activeclass tests

--- src/components/Todo/__tests__/Todo.test.js
+++ src/components/Todo/__tests__/Todo.test.js
@@ -10,17 +10,51 @@ const MockTodo = () => {
   );
 };
 
-describe('Todo', () => {
-  it('should render todo list after input and button event', () => {
-    render(<MockTodo />);
-    const inputElement = screen.getByPlaceholderText(/Add a new task here/i);
-    const buttonElement = screen.getByRole('button', { name: /Add/i });
-
-    fireEvent.change(inputElement, { target: { value: 'Grocery Shopping' } });
+const addTask = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here/i);
+  const buttonElement = screen.getByRole('button', { name: /Add/i });
+  tasks.forEach((task) => {
+    fireEvent.change(inputElement, {
+      target: { value: task },
+    });
     fireEvent.click(buttonElement);
+  });
+};
+
+describe('Todo', () => {
+  it('should render single todo list item after input and button event', () => {
+    render(<MockTodo />);
+    addTask(['Grocery Shopping']);
 
     const divElement = screen.getByText(/Grocery Shopping/i);
 
     expect(divElement).toBeInTheDocument();
+  });
+
+  it('should render multiple todo list items after input and button event', () => {
+    render(<MockTodo />);
+
+    addTask(['Grocery Shopping', 'Wash Hands', 'Pet Cat']);
+    const divElement = screen.getAllByTestId('task-container');
+
+    expect(divElement.length).toBe(3);
+  });
+
+  it('should  have "todo-item-active" class when added to list', () => {
+    render(<MockTodo />);
+
+    addTask(['Grocery Shopping']);
+    const divElement = screen.getByText(/Grocery Shopping/i);
+    fireEvent.click(divElement);
+    expect(divElement).toHaveClass('todo-item-active');
+  });
+
+  it('should not have "todo-item-active" class when initially rendered', () => {
+    render(<MockTodo />);
+
+    addTask(['Grocery Shopping']);
+    const divElement = screen.getByText(/Grocery Shopping/i);
+
+    expect(divElement).not.toHaveClass('todo-item-active');
   });
 });
